f647303dd9b18bb4a89895cc494afc4a554d1bab
early evening <eve@haut-kultur.org>
2019-04-16 15:24:51 -0600

    example usage of client - compiles and runs!
    🌹🐡

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+extern crate ridb_client;
+extern crate futures;
+
+use ridb_client::apis::*;
+
 fn main() {
-    println!("Hello, world!");
+    let api_key : String = match std::env::var("RIDB_API_KEY") {
+        Ok(val) => val,
+        Err(e) => panic!("no api key: {}", e),
+    };
+
+    let api_key_struct = configuration::ApiKey{
+        prefix: None,
+        key: api_key,
+    };
+
+    let mut config = configuration::Configuration::new();
+
+    config.api_key = Some(api_key_struct);
+
+    let c = client::APIClient::new(config);
+    let p = c.permit_entrances_api();
+
+    let permit_entr_id = "376";
+    let res = p.get_permit_entrance(&permit_entr_id);
+
+    match res {
+      Ok(ent) => println!("Got body: "),
+      Err(e) => println!("Got error: {}", error_str(e)),
+    }
+
+    println!("Done!");
+}
+
+fn error_str(e: ridb_client::apis::Error) -> String {
+    return match e {
+        ridb_client::apis::Error::Reqwest(e) => format!("uri error: {}", e),
+        ridb_client::apis::Error::Serde(e) => format!("encoding error: {}", e),
+    }
 }
